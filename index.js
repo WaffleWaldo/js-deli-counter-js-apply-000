51790fadd2653f3d867720b57633c9c09dccac43
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-10 14:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 function takeANumber(line, newName){
-  var placement = line.length + 1 
   line.push(newName)
   
-  return `Welcome, ${newName}. You are number ${placement} in line.`
+  return `Welcome, ${newName}. You are number ${line.length} in line.`
 }
 
 function nowServing(line){
-  line = line.shift()
+  var next = line.shift()
   
-  if (line === undefined){
+  if (next === undefined){
     return "There is nobody waiting to be served!"
-  }
-  else{
-    return `Currently serving ${line}.`
+  }else{
+    return `Currently serving ${next}.`
   }
 }
 
 function currentLine(line){
   if (line.length === 0){
     return 'The line is currently empty.'
-  }
-  else{
+  }else{
     var placementName = []
     for(let i=0;i<line.length; i++){
       placementName.push(`${i+1}. ${line[i]}`)
